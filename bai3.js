f880bbec2bed7f00bfb673d3e1c3c88a54d3ff6e
Quân Trần <quanlatoi199x@gmail.com>
2019-02-21 21:44:19 +0700

    sửa lại code

--- bai3.js
+++ bai3.js
@@ -1,60 +1,73 @@
-
+var listCard = document.querySelector('.listCard');
 var addCard = document.getElementById('add');
-addCard.addEventListener('click', function() {
-  var showTextArea = document.getElementById('add');
-  showTextArea.setAttribute('style', 'display: none');
-  var hiddenAdd = document.getElementById('text');
-  hiddenAdd.removeAttribute('style', 'display: block');
-  var btn = document.getElementById('btn');
-  //add event cho thẻ input button
-  btn.addEventListener('click', btnClick);
-  //add event cho thẻ div chứa icon X
-  var iconX = document.querySelector('.icon');
-  iconX.addEventListener('click', function() {
-    var showAdd = document.getElementById('add');
-    showAdd.removeAttribute('style', 'display: none');
-    var hiddenText = document.getElementById('text');
-    hiddenText.setAttribute('style', 'display: none');
-    document.querySelector('textarea').value = '';
-  });
-});
-var idName = 1;
+var text = document.getElementById('text');
+var iconX = document.querySelector('.icon');
+var btn = document.getElementById('btn');
+var textContent = document.querySelector('textarea');
 
-function btnClick() {
-  var classListCard = document.querySelector('.listCard');
-  idName++;
-  var idUseDrag = 'drag' + idName;
-  //tạo thẻ div
-  var text = document.querySelector('textarea').value;
-  if (text.length > 0) {
-    var div = document.createElement('div');
-    classListCard.appendChild(div);
-    div.setAttribute('class', 'tag');
-    // tạo thẻ a
-    var a = document.createElement('a');
-    div.appendChild(a);
-    a.setAttribute('class', 'father');
-    a.setAttribute('href', '#');
-    a.setAttribute('id', idUseDrag);
-    a.setAttribute('draggable', 'true');
-    //tạo thẻ span chứa icon;
-    var span = document.createElement('span');
-    a.appendChild(span);
-    span.setAttribute('class', 'child1');
-    //thẻ chứa tiêu đề
-    var span2 = document.createElement('span');
-    a.appendChild(span2);
-    span2.setAttribute('class', 'child2');
-    span2.textContent = text;
-    //trả về giao diện thêm thẻ ban đầu
-    var showAdd = document.getElementById('add');
-    showAdd.removeAttribute('style', 'display: none');
-    var hiddenText = document.getElementById('text');
-    hiddenText.setAttribute('style', 'display: none');
-    document.querySelector('textarea').value = '';
+addCard.addEventListener('click', function(){
+  hiddenElemnt(add);
+  showElement(text);
+});
+
+iconX.addEventListener('click', funIconX);
+
+btn.addEventListener('click', function(){
+  var content = textContent.value;
+  if(content.length > 0){
+    createA();
+    funIconX();
   }
-  document.getElementById('drag' + idName).addEventListener('dragstart', drag);
+});
+
+function showElement(element){
+  return element.removeAttribute('style', 'display: none');
 }
+
+function hiddenElemnt(element){
+  return element.setAttribute('style', 'display: none');
+}
+
+function funIconX(){
+  hiddenElemnt(text);
+  showElement(add);
+  textContent.value = '';
+}
+
+
+function createSpanText(){
+  var content = textContent.value;
+  var spanText =  document.createElement('span');
+  spanText.setAttribute('class', 'child2');
+  spanText.textContent = content;
+  return spanText;
+}
+
+function createSpan(){
+  var span = document.createElement('span');
+  span.setAttribute('class', 'child1');
+  return span;
+}
+
+function createA(){
+  var a = document.createElement('a');
+  a.setAttribute('class', 'father');
+  a.setAttribute('href', '#');
+  a.setAttribute('draggable', 'true');
+  a.appendChild(createSpanText());
+  a.appendChild(createSpan());
+  createDiv().appendChild(a);
+  a.addEventListener('dragstart', drag);
+  return a;
+}
+
+function createDiv(){
+  var div = document.createElement('div');
+  div.setAttribute('class', 'tag');
+  return listCard.appendChild(div);
+}
+
+
 
 document.getElementById('dragDemo1').addEventListener('dragstart', drag);
 document.getElementById('dragDemo2').addEventListener('dragstart', drag);
